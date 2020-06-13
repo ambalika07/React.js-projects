009fa125baaba1c4f2c24748211dd32991b75e57
Ambalik Katoch <ambalika.katoch@gmail.com>
2020-06-13 14:31:19 -0700

    new project - 'pics' created

--- seasons-p2/src/index.js
+++ seasons-p2/src/index.js
@@ -27,7 +27,7 @@ class App extends React.Component {
   }
 
   // React says we have to define render!!
-  render() {
+  renderContent() {
     if(this.state.errorMessage && !this.state.lat) {
     return <div>Error: {this.state.errorMessage}</div>
     }
@@ -37,6 +37,10 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request"/>;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>
   }
 }
 
