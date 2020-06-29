63d8d838992ce9d59d3f7e10c1d453f68cea619b
Ambalik Katoch <ambalika.katoch@gmail.com>
2020-06-29 11:51:57 -0700

    List building

--- blog-p6/src/action/index.js
+++ blog-p6/src/action/index.js
@@ -1,5 +1,12 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    }
-}
+import jasonPlaceholder from '../apis/jasonPlaceholder';
+
+export const fetchPosts = () => async (dispatch, getState) => {
+        const response = await jasonPlaceholder.get('/posts')
+
+        dispatch({ type: 'FETCH_POSTS', payload: response.data})
+
+        // return {
+        //     type: 'FETCH_POSTS',
+        //     payload: promise
+        // };
+    };
